8471bb7a4373bb14dc00766f08e3c863c6bf3baf
Minter <adf5858@naver.com>
2025-08-20 01:36:09 +0800

    Feat: 자산 이동 관련 DTO 정의

---
+++ app/types/movement.ts
@@ -0,0 +1,84 @@
+import { UserType } from './common';
+import { AssetDto, AssetType } from './asset';
+
+// MARK: - 자산 이동 이력 관련
+
+// 자산 이동 타입
+export type HistoryChangeType = 'type' | 'supplier' | 'brand' | 'serial number' | 'purchase date' | 'purchase order number' | 'warranty start date' | 'warranty end date' | 'category'
+
+// 대출 요청
+export interface RentRequest {
+    assetId: string;
+    userId: string;
+}
+
+// 대출 응답
+export interface RentResponse {
+    success: boolean;
+    message?: string;
+}
+
+// 반납 요청
+export interface LoanRequest {
+    assetId: string;
+}
+
+// 반납 응답
+export interface LoanResponse {
+    success: boolean;
+    message?: string;
+}
+
+
+// 관리자용 히스토리 개별 항목
+export interface AdminHistoryDto {
+    historyId: string;
+    date: Date;
+    changeType: HistoryChangeType;
+    description: string;
+}
+
+// 관리자용 특정 상품 이동 이력 조회
+export interface AdminMovementListDto {
+    assetId: string;
+    assetInfo: {
+        name: string;
+        serialNumber: string;
+        description: string;  // 변경사항 표시
+    }
+    histories: AdminHistoryDto[];
+}
+
+
+// 일반 사용자용 이동 이력
+export interface UserMovementListDto {
+    date: Date;
+    changeType: HistoryChangeType;
+    assetInfo: {
+        name: string;
+        serialNumber: string;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
