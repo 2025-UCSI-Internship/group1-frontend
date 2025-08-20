aa32c2a2ef91964d585e153a50d0eec86237a2fc
Minter <adf5858@naver.com>
2025-08-20 15:04:58 +0800

    Revert "WIP: api 중앙 export 파일 등록"
    This reverts commit 74752a0e4bffacc933142c617e8a9f6401c1c201.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,5 @@
 export * from './asset';
-// export * from './auth'; TODO: - 예현, 현정 작업 내용 확인 후 주석 해제
+// export * from './auth';
 export * from './common';
 export * from './movement';
 export * from './notification';
