c6bf4035387b0bfde2eae0538f1b10d9bd2da851
Minter <adf5858@naver.com>
2025-08-20 19:25:42 +0800

    Style: 불필요한 주석 제거

--- app/utils/api/asset.ts
+++ app/utils/api/asset.ts
@@ -1,4 +1,4 @@
-// MARKL - 자산 관련 API 호출
+// MARK: - 자산 관련 API 호출
 
 import { apiClient } from './client';
 import { API_ENDPOINTS } from '~/constants/endpoints';
@@ -32,7 +32,7 @@ export const assetAPI = {
 
 
     // 자산 수정
-    udpateAsset: async (assetId: string, data: UpdateAssetRequestDto) => {
+    updateAsset: async (assetId: string, data: UpdateAssetRequestDto) => {
         const url = API_ENDPOINTS.ASSET.UPDATE.replace(':asset_id', assetId);
         const response = await apiClient.put(url, data); // TODO: - api 명세서 메서드 확인 필요
         return response.data;
@@ -40,7 +40,7 @@ export const assetAPI = {
 
     // 자산 목록 조회
     getAssets: async (params?: GetAssetsRequestDto) => {
-        const response = await apiClient.get(API_ENDPOINTS.ASSET.LIST, { params }); // 왜 여기 params는 중괄호 사용?
+        const response = await apiClient.get(API_ENDPOINTS.ASSET.LIST, { params });
         return response.data;
     },
 
@@ -49,12 +49,8 @@ export const assetAPI = {
         const url = API_ENDPOINTS.ASSET.DETAIL.replace(':assetId', assetId);
         const response = await apiClient.get(url);
         return response.data;
-    },// 왜 여기 끝에서는 다 콤마로 찍는지 
+    },
 
 };
 
 
-
-/* export 는 뭐지 
-여기서도 세미콜론과 콤마를 언제 사용하는지 굉장히 헷갈리네
-*/
