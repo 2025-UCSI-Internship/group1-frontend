ea5fa9614ba3e8ce6c751c30c8e38685fdda2543
Minter <adf5858@naver.com>
2025-08-20 19:32:26 +0800

    Feat: 자산 이동 API 추가 및 주석 추가

--- app/utils/api/index.ts
+++ app/utils/api/index.ts
@@ -1,3 +1,5 @@
 export { apiClient } from './client';
 export { assetAPI } from './asset';
 // export { authAPI } from './auth'; TODO: - 서연, 예진 업무 종료시 주석 해제 + 중앙 export 
+export { movementAPI } from './movement';
+// TODO: - 예현 알림 axios API 구성후 추가
